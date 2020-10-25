9de1bbdfd328c1321607a467ce69f5abb486e9b6
sirajbd0176 <sirajbd0176@gmail.com>
2020-10-25 18:35:03 +0100

    fix: deploy to heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 var path = require('path');
 
 app.use('/', express.static(path.join(__dirname, 'public')));
